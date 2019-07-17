a08ad7af6038a143eba6746172f5a5ff145a2231
longjie <761166384@qq.com>
2019-07-17 22:41:00 +0800

    Default Changelist

--- testEcharts/src/app/moment/moment.component.ts
+++ testEcharts/src/app/moment/moment.component.ts
@@ -31,7 +31,66 @@ export class MomentComponent implements OnInit {
         t8: null,
         t9: null,
         t10: null,
-        t11: null
+        t11: null,
+        t12: null,
+        t13: null,
+        t14: null,
+        t15: null,
+        t16: null,
+        t17: null,
+        t18: null,
+        t19: null,
+        t20: null,
+        t21: null,
+        t22: null,
+        t23: null,
+        t24: null,
+        t25: null,
+        t26: null,
+        t27: null,
+        t28: null,
+        t29: null,
+        t30: null,
+        t31: null,
+        t32: null,
+        t33: null
+    };
+    setTime = {
+        s1: null,
+        s2: null,
+        s3: null,
+        s4: null,
+        s5: null,
+        s6: null,
+        s7: null,
+        s8: null,
+        s9: null,
+        s10: null,
+        s11: null,
+        s12: null,
+        s13: null,
+        s14: null,
+        s15: null,
+        s16: null,
+        s17: null,
+        s18: null,
+        s19: null,
+        s20: null,
+        s21: null,
+        s22: null,
+        s23: null,
+        s24: null,
+        s25: null,
+        s26: null,
+        s27: null,
+        s28: null,
+        s29: null,
+        s30: null,
+        s31: null,
+        s32: null,
+        s33: null
+
+
     };
 
     constructor(private message: NzMessageService) {
@@ -59,7 +118,36 @@ export class MomentComponent implements OnInit {
         this.getTime.t7 = moment().endOf('isoWeek');
         this.getTime.t8 = moment().endOf('month');
         this.getTime.t9 = moment().daysInMonth();
+        this.getTime.t10 = moment().format('X');
+        this.getTime.t11 = moment().unix();
+        this.getTime.t12 = moment().format('x');
+        this.getTime.t13 = moment().valueOf();
+        this.getTime.t14 = moment().year();
+        this.getTime.t15 = moment().get('year');
+        this.getTime.t16 = moment().month();
+        this.getTime.t17 = moment().get('month');
+        this.getTime.t18 = moment().date();
+        this.getTime.t19 = moment().get('date');
+        this.getTime.t20 = moment().day();// (0~6,0:sunday,6:saturday)
+        this.getTime.t21 = moment().weekday(); // (0~6,0:sunday,6:staurday)
+        this.getTime.t22 = moment().isoWeekday(); // (1~7,1:monday,7:sunday)
+        this.getTime.t23 = moment().get('day');
+        this.getTime.t24 = moment().get('weekday');
+        this.getTime.t25 = moment().get('isoWeekday');
+        this.getTime.t26 = moment().hours();
+        this.getTime.t27 = moment().get('hours');
+        this.getTime.t28 = moment().minutes();
+        this.getTime.t29 = moment().get('minutes');
+        this.getTime.t30 = moment().seconds();
+        this.getTime.t31 = moment().get('seconds');
+        // [years,months,date,hours,minutes,seconds,milliseconds]
+        this.getTime.t32 = moment().toArray();
+        // {years:xxx,months:x,date:xx...}
+        this.getTime.t33 = moment().toObject();
+
+
     }
+
 
     pre() {
         this.current -= 1;
